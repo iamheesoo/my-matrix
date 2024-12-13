c491cda66d57fa5b7b69437b2cd4f5ed79eb1c8f
iamheesoo <beforeheesoo@gmail.com>
2024-12-13 14:15:23 +0900

    [Add] 데이터를 가지고 화면 이동

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_matrix/page/SecondPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,146 +11,72 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'My Matrix'),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
+      appBar: AppBar(
+        title: Text(
+          "hello",
+          style: TextStyle(color: Colors.red),
         ),
-        body: LayoutBuilder(
-          builder: (context, constraints) {
-            // 화면의 너비와 높이 가져오기
-            double itemWidth = constraints.maxWidth / 2;
-            double itemHeight = constraints.maxHeight / 2;
-
-            return Column(
+      ),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            child: Column(
               children: [
-                Row(
-                  children: [
-                    Container(
-                      width: itemWidth,
-                      height: itemHeight,
-                      color: Colors.blue,
-                      child: Center(
-                        child: Text(
-                          'Item 1',
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      width: itemWidth,
-                      height: itemHeight,
-                      color: Colors.red,
-                      child: Center(
-                        child: Text(
-                          'Item 2',
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                        ),
-                      ),
-                    ),
-                  ],
+                Expanded(
+                  flex: 1,
+                  child: Container(color: Colors.purple),
                 ),
-                Row(
-                  children: [
-                    Container(
-                      width: itemWidth,
-                      height: itemHeight,
-                      color: Colors.green,
-                      child: Center(
-                        child: Text(
-                          'Item 3',
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      width: itemWidth,
-                      height: itemHeight,
-                      color: Colors.orange,
-                      child: Center(
-                        child: Text(
-                          'Item 4',
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                        ),
-                      ),
-                    ),
-                  ],
+                Expanded(
+                  flex: 1,
+                  child: Container(color: Colors.pink),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(color: Colors.yellow),
                 ),
               ],
-            );
-          },
-        ) // This trailing comma makes auto-formatting nicer for build methods.
-        );
+            ),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.green,
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.blue,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => SecondPage(
+                            isKorean: false,
+                            name: "JOHN",
+                            age: 20,
+                            gender: "F",
+                          )));
+            },
+            child: Container(
+              child: Text("goToSecond"),
+              color: Colors.grey,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
