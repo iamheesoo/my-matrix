480b9548669a73c97cfec01a71debb594fbc27a7
iamheesoo <beforeheesoo@gmail.com>
2025-01-15 14:31:12 +0900

    [Add] 체크박스 추가

--- lib/page/check_list_view.dart
+++ lib/page/check_list_view.dart
@@ -1,36 +1,58 @@
 import 'package:flutter/material.dart';
 
-class CheckListView extends StatelessWidget {
+class CheckListView extends StatefulWidget {
   final List<String> checkList;
   final String title;
 
   const CheckListView({required this.checkList, required this.title, super.key});
 
   @override
+  _CheckListViewState createState() => _CheckListViewState();
+}
+
+class _CheckListViewState extends State<CheckListView> {
+  late List<bool> _checked;
+
+  @override
+  void initState() {
+    super.initState();
+    _checked = List.generate(widget.checkList.length, (index) => false);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(children: [
       Text(
-        title,
+        widget.title,
         style: const TextStyle(fontWeight: FontWeight.bold),
       ),
       Expanded(
           child: Container(
-        margin: const EdgeInsets.all(8),
-        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-        child: ListView.builder(
-          itemBuilder: (BuildContext context, int position) {
-            return getContainer(context, position);
-          },
-          itemCount: checkList.length,
-        ),
-      )),
+            margin: const EdgeInsets.all(8),
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+            child: ListView.builder(
+              itemBuilder: (BuildContext context, int position) {
+                return getContainer(context, position);
+              },
+              itemCount: widget.checkList.length,
+            ),
+          )),
     ]);
   }
 
   Container getContainer(BuildContext context, int position) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      child: Text("$position"),
+      child: Row(
+        children: [
+          Checkbox(value: _checked[position], onChanged: (bool? isChecked) {
+                setState(() {
+                  _checked[position] = isChecked ?? false;
+                });
+          }),
+          Text("$position")
+        ],
+      ),
     );
   }
 }
